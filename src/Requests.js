f8c4d7d6b566d0123adb0eff28fa01eac9f0d101
Ajay Nishad <ajaynishad9167@gmail.com>
2023-02-13 15:09:39 +0530

    Netflix Clone

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = '7430c6a801992159658b666cfe9c80a8';
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requsetUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+}
+
+export default requests;
